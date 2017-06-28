4007a85601073bdb6fb326d2f5c30a848b78cf7f
anatoliy.rugalev <anatoliy.rugalev@gs-labs.ru>
2017-06-28 21:40:43 +0200

    Implemented variadic run experiment

--- example/main.go
+++ example/main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"fmt"
 	"github.com/earlcherry/gouter/src"
+	"os"
+	"reflect"
 )
 
 func main() {
+	args := os.Args[1:]
 	g := gouter.New()
-	g.Add("test", YourStruct{},"Method")
-	g.Add("test23", YourStruct{},"")
-	g.Add("test3", YourStruct{},"")
-	g.Run("test")
+	g.Add("test", YourStruct{}, "Method")
+	g.Add("test23", YourStruct{}, "")
+	g.Add("test3", YourStruct{}, "")
+
+	inputs := make([]reflect.Value, len(args))
+	for i := 0; i < len(args); i++ {
+		inputs[i] = reflect.ValueOf(args[i])
+	}
+	if len(inputs) < 2 {
+		panic("Not enough arguments")
+	}
+
+	reflect.ValueOf(g).MethodByName("Run").Call(inputs)
 }
 
 type YourStruct struct{}
 
-func (y YourStruct) Method() {
+func (y YourStruct) Method(a string, b string, c string) {
+	fmt.Printf(a)
+	fmt.Printf(b)
+	fmt.Printf(c)
 	fmt.Printf("Yep, its work!")
 }
--- src/gouter.go
+++ src/gouter.go
@@ -1,26 +1,44 @@
 package gouter
 
+import (
+	"reflect"
+)
+
 type Gouter struct {
-	routes map[string]Gout
+	routes map[string]Goute
 }
-type Gout struct {
+
+type Goute struct {
 	Controller interface{}
 	Method     string
 }
 
 func New() *Gouter {
 	return &Gouter{
-		routes: make(map[string]Gout),
+		routes: make(map[string]Goute),
 	}
 }
 
-func (g *Gouter) Add(rout string, controller interface{}, method string) {
-	g.routes[rout] = Gout{
+func (g *Gouter) Add(route string, controller interface{}, method string) {
+	g.routes[route] = Goute{
 		Controller: controller,
 		Method:     method,
 	}
 }
-func (g *Gouter) Run(rout string) {
-	gout := g.routes[rout]
-	Call(gout.Controller, gout.Method)
+func (g *Gouter) Run(route string, args... string) {
+	goute := g.routes[route]
+
+	method := reflect.ValueOf(goute.Controller).MethodByName(goute.Method)
+	argsCount := method.Type().NumIn()
+
+	inputs := make([]reflect.Value, argsCount)
+	for i := 0; i < argsCount; i++ {
+		if i >= len(args) {
+			inputs[i] = reflect.ValueOf("")
+		} else {
+			inputs[i] = reflect.ValueOf(args[i])
+		}
+	}
+
+	method.Call(inputs)
 }
